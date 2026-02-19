84158b676715c9f85a3766f30b078b3588dde569
David Marom <davidmarom@hotmail.com>
2026-02-19 19:42:13 +0200

    service

--- src/app/computer/page.jsx
+++ src/app/computer/page.jsx
@@ -1,39 +1,26 @@
 'use client';
 
 import React, { useState, useEffect } from 'react';
-// import { getComputers } from '@/services/data.js';
 import { Card } from '@/app/components/Card/Card.jsx';
+import { getComputers } from '@/services/compProd.js';
 
 
 export default function Home() {
     const [items, setItems] = useState([]);
 
-    useEffect(
-        () => {
-            fetch('/api/items')
-                .then(res => res.json())
-                .then(data => setItems(data))
-                .catch(err => console.error(err));
-        },
-        []
-    )
-
+    useEffect(() => { getComputers().then(res => setItems(res)) }, [])
 
     return (
         <div className='page-container '>
             <h1>Computer Page</h1>
 
             <div className="grid">
-
                 {items.map(
                     (item, idx) => {
                         return <Card key={idx} data={item} />
                     }
                 )}
-            
-
             </div>
-
         </div>
     );
 }
---
+++ src/services/compProd.js
@@ -0,0 +1,6 @@
+
+export const getComputers = () => {
+  return fetch('/api/items')
+    .then(res => res.json())
+    .catch(err => console.error(err));
+}
