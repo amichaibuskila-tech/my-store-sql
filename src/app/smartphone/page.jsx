8b0f5cf09d8d4caaeb74b7278b3be9832e7623da
Amichai Buskila <amichai.buskila@gmail.com>
2026-02-26 19:34:04 +0200

    aa

---
+++ src/app/smartphone/page.jsx
@@ -0,0 +1,27 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { Card } from '@/app/components/Card/Card.jsx';
+import { getItemsByCategory } from '@/services/itemsClient.js';
+
+export default function SmartPhone() {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => { getItemsByCategory('electric').then(res => setItems(res)) }, [])
+
+
+
+
+    return (
+        <div className="smartphone-page page-container">
+            <h1>SmartPhone Page</h1>
+            <div className="grid">
+                {items.map(
+                    (item, idx) => {
+                        return <Card key={idx} data={item} />
+                    }
+                )}
+            </div>
+        </div>
+    );
+}
---
+++ src/app/smartwatch/page.jsx
@@ -0,0 +1,29 @@
+'use client';
+
+import React, { useState, useEffect } from 'react';
+import { Card } from '@/app/components/Card/Card.jsx';
+import { getItemsByCategory } from '@/services/itemsClient.js';
+
+
+export default function SmartWatch() {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => { getItemsByCategory('electric').then(res => setItems(res)) }, [])
+
+
+
+
+
+    return (
+        <div className="smartwatch-page page-container">
+            <h1>SmartWatch Page</h1>
+                        <div className="grid">
+                            {items.map(
+                                (item, idx) => {
+                                    return <Card key={idx} data={item} />
+                                }
+                            )}
+        </div>
+        </div>
+    );
+}
